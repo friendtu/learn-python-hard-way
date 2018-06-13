ed36cc643846107f16aa10f7832a0d50cdbd9cac
tud <friendtu@gmail.com>
2018-06-13 17:23:42 +0800

    ex11

---
+++ ex11.py
@@ -0,0 +1,15 @@
+print "\tI'm tabbed in."
+print "I'm split\non a line."
+print "I'm \\ a \\ cat."
+
+print '''
+I"""ll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip
+\t* Grass
+'''
+
+#while True:
+#        for i in ["/","-","|","\\","|"]:
+#            print "%s\r" % i,
