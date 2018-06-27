24d9a9822227fa330f2363add5cf65b849f1dd82
friendtu <friendtu@gmail.com>
2018-06-27 19:08:46 +0800

    ex11-14

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print "How old are your?"
+age=raw_input("Year:")
+
+print "How tall are you?"
+height=raw_input("CM:")
+
+print "How much do you weigh?"
+weigh=raw_input("KG:")
+
+print "So, you're %s year old, %s CM tall and %s KG heavy" % (
+    age,height,weigh)
