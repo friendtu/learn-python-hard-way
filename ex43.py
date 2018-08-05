ebd6a8fa8dc932e6630e7fe4811e9f3cb5b53031
friendtu <friendtu@gmail.com>
2018-08-05 14:13:41 +0800

    update class

--- ex43.py
+++ ex43.py
@@ -1,13 +1,20 @@
 class Animal(object):
-    pass
+    def sound(self):
+        print "sound"
 
 class Dog(Animal):
     def __init__(self,name):
         self.name=name
     
+    def sound(self):
+        print "WangWang"
+    
 class Cat(Animal):
     def __init__(self,name):
         self.name=name
+
+    def sound(self):
+        print "%s MiaoMiao" % self.name
 
 class Person(object):
     def __init__(self,name):
@@ -31,12 +38,14 @@ class Halibut(Fish):
 rover=Dog("Rover")
 
 satan=Cat("Satan")
+satan.sound()
 
 mary=Person("Mary")
 mary.pet=satan
 
 frank=Employee("Frank",12000)
 frank.pet=rover
+print frank.name
 
 flipper=Fish()
 
