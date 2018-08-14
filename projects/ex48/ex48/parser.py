31963defc9b253747211471199e0a9cbcd8193d4
friendtu <friendtu@gmail.com>
2018-08-14 21:42:20 +0800

    add testing of parser

--- projects/ex48/ex48/parser.py
+++ projects/ex48/ex48/parser.py
@@ -1,1 +1,59 @@
+
+class ParseError(Exception):
+    pass
+    
+class Sentence(object):
+    def __init__(self,subject,verb,obj):
+        if(subject==None):
+             self.subject=None
+        else:
+            self.subject=subject[1]
+        self.verb=verb[1]
+        self.obj=obj[1]
+
+def peektype(word_list):
+    if word_list==None: return None
+    word=word_list[0]
+    return word[0]
+
+def skip_stop(word_list):
+    while(word_list and peektype(word_list)=='stop'):
+        parse_one(word_list,'stop')
+
+def parse_one(word_list,type):
+    if word_list==None: return None
+
+    if(peektype(word_list)==type):
+        return word_list.pop(0)
+    else:
+        return None
+
+def parse_subject(word_list):
+    skip_stop(word_list)
+    word=parse_one(word_list,'noun')
+    if word==None:
+        word=('noun','player')
+    return word
+    
+def parse_verb(word_list):
+    skip_stop(word_list)
+    word=parse_one(word_list,'verb')
+    if word==None:
+        raise ParseError("Expected a verb next.")
+    else: 
+        return word
+
+def parse_object(word_list):
+    skip_stop(word_list)
+    word=parse_one(word_list,'direction')
+    if word==None:
+        word=parse_one(word_list,'noun')
+        if word==None:
+            raise ParseError("Expected a noun or direction")
+    return word
+
 def parse_sentence(word_list):
+    subject=parse_subject(word_list)
+    verb=parse_verb(word_list)
+    object=parse_object(word_list)
+    return Sentence(subject,verb,object)
--- projects/ex48/tests/parser_tests.py
+++ projects/ex48/tests/parser_tests.py
@@ -2,9 +2,38 @@ from nose.tools import *
 from ex48 import lexicon,parser
 
 
-def test_parser():
+def test_parser_noun_verb_object():
     word_list=lexicon.scan("bear go north")
-    sentence=parse_sentence(word_list)
+    sentence=parser.parse_sentence(word_list)
     assert_equal(sentence.subject,"bear")
     assert_equal(sentence.verb,'go')
-    assert_equal(object,'north')
+    assert_equal(sentence.obj,'north')
+
+def test_parser_stop_noun_verb_object():
+    word_list=lexicon.scan("the bear go north")
+    sentence=parser.parse_sentence(word_list)
+    assert_equal(sentence.subject,"bear")
+    assert_equal(sentence.verb,'go')
+    assert_equal(sentence.obj,'north')
+
+def test_parser_verb_object():
+    word_list=lexicon.scan("go north")
+    sentence=parser.parse_sentence(word_list)
+    assert_equal(sentence.subject,'player')
+    assert_equal(sentence.verb,'go')
+    assert_equal(sentence.obj,'north')
+
+def test_parser_verb_stop_object():
+    word_list=lexicon.scan("go the north")
+    sentence=parser.parse_sentence(word_list)
+    assert_equal(sentence.subject,'player')
+    assert_equal(sentence.verb,'go')
+    assert_equal(sentence.obj,'north')
+
+@raises(parser.ParseError)
+def test_parser_verb_error_object():
+    word_list=lexicon.scan("go there north")
+    sentence=parser.parse_sentence(word_list)
+    assert_equal(sentence.subject,'player')
+    assert_equal(sentence.verb,'go')
+    assert_equal(sentence.obj,'north')
