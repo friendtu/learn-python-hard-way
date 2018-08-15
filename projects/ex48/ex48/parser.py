0d07738bb862bd55a99c8ad67e7c54cbbad0a58d
Donnie Tu <donnie.tu@autodesk.com>
2018-08-15 11:03:23 +0800

    move to class - middle

--- projects/ex48/ex48/parser.py
+++ projects/ex48/ex48/parser.py
@@ -11,49 +11,53 @@ class Sentence(object):
         self.verb=verb[1]
         self.obj=obj[1]
 
-def peektype(word_list):
-    if word_list==None: return None
-    word=word_list[0]
-    return word[0]
-
-def skip_stop(word_list):
-    while(word_list and peektype(word_list)=='stop'):
-        parse_one(word_list,'stop')
-
-def parse_one(word_list,type):
-    if word_list==None: return None
-
-    if(peektype(word_list)==type):
-        return word_list.pop(0)
-    else:
-        return None
-
-def parse_subject(word_list):
-    skip_stop(word_list)
-    word=parse_one(word_list,'noun')
-    if word==None:
-        word=('noun','player')
-    return word
+class Parser(object):
+    def __init__(self,word_list):
+        self.word_list=word_list
     
-def parse_verb(word_list):
-    skip_stop(word_list)
-    word=parse_one(word_list,'verb')
-    if word==None:
-        raise ParseError("Expected a verb next.")
-    else: 
+    def peektype(self):
+        if self.word_list==None: return None
+            word=self.word_list[0]
+        return word[0]
+
+    def skip_stop(self):
+        while(self.word_list and peektype()=='stop'):
+            match_one(self.word_list,'stop')
+
+    def parse_one(self,type):
+        if self.word_list==None: return None
+
+        if(peektype()==type):
+            return self.word_list.pop(0)
+        else:
+            return None
+
+    def parse_subject(self):
+        skip_stop()
+        word=match_one('noun')
+        if word==None:
+            word=('noun','player')
+        return word
+    
+    def parse_verb(self):
+        skip_stop()
+        word=match_one('verb')
+        if word==None:
+            raise ParseError("Expected a verb next.")
+        else: 
+            return word
+
+    def parse_object(self):
+        skip_stop()
+        word=match_one('direction')
+        if word==None:
+            word=parse_one('noun')
+            if word==None:
+                raise ParseError("Expected a noun or direction")
         return word
 
-def parse_object(word_list):
-    skip_stop(word_list)
-    word=parse_one(word_list,'direction')
-    if word==None:
-        word=parse_one(word_list,'noun')
-        if word==None:
-            raise ParseError("Expected a noun or direction")
-    return word
-
-def parse_sentence(word_list):
-    subject=parse_subject(word_list)
-    verb=parse_verb(word_list)
-    object=parse_object(word_list)
-    return Sentence(subject,verb,object)
+    def parse_sentence(self):
+        subject=parse_subject()
+        verb=parse_verb()
+        object=parse_object()
+        return Sentence(subject,verb,object)
